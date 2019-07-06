d43d8002bcdedf4cbf357075e69af30e26c94ada
Sandra Vera <sandrav0609@gmail.com>
2019-07-06 14:04:40 -0400

    add templates dir settings

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('to-do/', include('ToDoListMiniApp.urls')),
+    path('', include('ToDoListMiniApp.urls')),
 ]
