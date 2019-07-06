f7ab6b592821c586ade171663aa8adf12bfa6467
Sandra Vera <sandrav0609@gmail.com>
2019-07-06 17:58:51 -0400

    add login decorator

--- ToDoListMiniApp/views.py
+++ ToDoListMiniApp/views.py
@@ -3,10 +3,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 from .models import Todo
 from .forms import TodoForm
 
 # Create your views here.
+class sign_up(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+@login_required(login_url='/login/')
 def index(request):
     todo_list = Todo.objects.order_by('id')
     context = {
@@ -14,15 +21,12 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
-class sign_up(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
-
+@login_required(login_url='/login/')
 def detail(request, todo_id):
     todo = get_object_or_404(Todo, pk=todo_id)
     return render(request, 'detail.html', {'todo':todo})
 
+@login_required(login_url='/login/')
 def add_todo(request):
     if request.method == 'POST':
         form = TodoForm(request.POST)
