d601186219c0f769da823e0058dc43744f143653
Sandra Vera <sandrav0609@gmail.com>
2019-07-06 00:25:01 -0400

    add To-do form and fix settings

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ToDoApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates/ToDoListMiniApp")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
