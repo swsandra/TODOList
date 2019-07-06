e21cb0e8dcdf74892bd516d7c0a4b971b3caffec
Sandra Vera <sandrav0609@gmail.com>
2019-07-06 15:40:48 -0400

    add basic views and urls

--- ToDoListMiniApp/urls.py
+++ ToDoListMiniApp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:todo_id>/', views.detail, name='detail'),
+    path('add/', views.add_todo, name='form'),
 ]
--- ToDoListMiniApp/views.py
+++ ToDoListMiniApp/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    todo_list = Todo.objects.order_by('id')
+    context = {
+        'todo_list': todo_list,
+    }
+    return render(request, 'index.html', context)
+
+def detail(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    return render(request, 'detail.html', {'todo':todo})
+
+def add_todo(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = TodoForm()
+    return render(request, 'form.html', {'form': form})
