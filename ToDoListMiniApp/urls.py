33fd41e9b0b8607f6fe25039acec68bdd360c233
Sandra Vera <sandrav0609@gmail.com>
2019-07-07 14:55:54 -0400

    debug navbar

--- ToDoListMiniApp/urls.py
+++ ToDoListMiniApp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:todo_id>/', views.detail, name='detail'),
     path('signup/', views.sign_up, name='signup'),
-    path('add/', views.add_todo, name='form'),
+    path('add/', views.add_todo, name='add'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
