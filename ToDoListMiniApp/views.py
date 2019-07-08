8a0854ee39c152f91e46a4cc66b543910de8911e
Sandra Vera <sandrav0609@gmail.com>
2019-07-07 21:53:48 -0400

    implement edit to-do view and template

--- ToDoListMiniApp/views.py
+++ ToDoListMiniApp/views.py
@@ -70,7 +70,19 @@ def add(request):
 @login_required(login_url='/login/')
 def edit(request, todo_id):
     todo = get_object_or_404(Todo, pk=todo_id)
-    return render(request, 'detail.html', {'todo': todo})
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            for field in form:
+                for error in field.errors:
+                    err = '{}: {}'.format(field.label,error)
+                    messages.error(request,err)
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'edit.html', {'todo': todo, 'form': form})
 
 @login_required(login_url='/login/')
 def delete(request, todo_id):
