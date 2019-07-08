49d1c7da585ea0fce95b392b0329f4a266616290
Sandra Vera <sandrav0609@gmail.com>
2019-07-08 13:33:17 -0400

    fix bug for local run

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -132,9 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
